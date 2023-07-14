da346ab7a27ac00f949bb1ee06d15660cb2260cb
Talhaalpha <talha.alphasquad@gmail.com>
2023-07-14 15:24:48 +0500

    grid card test file added

---
+++ src/gridCardsTest.svelte
@@ -0,0 +1,26 @@
+<div class="gridContainer"> 
+    <div class="gridItem">grid item 1</div>
+    <div class="gridItem">grid item 2</div>
+    <div class="gridItem">grid item 3</div>
+    <div class="gridItem">grid item 1</div>
+    <div class="gridItem">grid item 2</div>
+    <div class="gridItem">grid item 3</div>
+</div>
+
+
+
+<style>
+.gridContainer{
+    display: grid;
+    /* width: 20%; */
+    grid-template-columns: auto auto auto;
+    grid-template-rows: 150px 150px;
+
+    background-color: bisque;
+    grid-gap: 10px;
+}
+.gridItem{
+    background-color: aquamarine;
+
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -131,8 +131,8 @@
             <div class="cardRow1">
                 <!-- Card 1 -->
                 {#each posts as post (post.id)}
-                    <div class="">
-                        <img src={post.url} alt="" />
+                    <div>
+                        <img class="cardImg" src={post.url} alt="" />
                         <p class="date">27 Jan 2021</p>
                         <p class="cardPara">
                             {post.title}
@@ -153,8 +153,8 @@
                                     d="M24.5303 6.53033C24.8232 6.23744 24.8232 5.76256 24.5303 5.46967L19.7574 0.696699C19.4645 0.403806 18.9896 0.403806 18.6967 0.696699C18.4038 0.989593 18.4038 1.46447 18.6967 1.75736L22.9393 6L18.6967 10.2426C18.4038 10.5355 18.4038 11.0104 18.6967 11.3033C18.9896 11.5962 19.4645 11.5962 19.7574 11.3033L24.5303 6.53033ZM0 6.75H24V5.25H0V6.75Z"
                                     fill="#282938"
                                 />
-                            </svg></a
-                        >
+                            </svg></a >
+                       
                     </div>
                 {/each}
             </div>
@@ -365,6 +365,13 @@
 </div>
 
 <style>
+
+
+
+    
+    /* .cardImg{
+        width: 50%;
+    } */
     .navLinks {
         color: white;
         text-decoration: none;
@@ -420,6 +427,7 @@
         /* align-items: center; */
         /* justify-content: center; */
         max-width: 50%;
+        width: 100%;
         margin-left: auto;
         margin-right: auto;
         /* flex-wrap: wrap; */
@@ -1772,6 +1780,16 @@ justify-content: center; */
 max-width: 1280px; 
         width: 100%;
     } */
+    .cardsDiv{
+        display: flex;
+        flex-direction: row;
+        /* align-items: center; */
+        /* justify-content: center; */
+        max-width: 50%;
+        /* margin-left: auto;
+        margin-right: auto; */
+        /* flex-wrap: wrap; */
+    }
 }
 
 </style>
